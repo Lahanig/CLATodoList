b44601a0f70d2d7fb66f7b026d04398f0a4c646f
Lahanig <lol355467@gmail.com>
2025-11-15 04:00:14 +0300

    Minor bug fix

--- modules/query_methods/list_all_todo.py
+++ modules/query_methods/list_all_todo.py
@@ -16,7 +16,7 @@ class Render:
             if group["id"] == loop_id:
                 if (loop_id != 0):
                     print(" ")
-                print(f'''{group["color"]}{group["text"]}{Fore.RESET}''')
+                print(f''' {group["color"]}{group["text"]}{Fore.RESET}''')
 
     def render_tasks(self, loop_id):
         tasks = storage.get_all_tasks()
@@ -24,18 +24,17 @@ class Render:
         i = 0
         for task in tasks:
             if task["group_id"] == loop_id:
-                temp_str = f'''{i+1}. {task["text"]}'''
-
                 if (task["is_completed"] == False):
-                    print(f''' {temp_str}''')
+                    print(f'''  {i+1}. {task["text"]}''')
                 else:
-                    print(f'''\033[9;30m {temp_str}\033[0m''')
+                    print(f'''  {i+1}. \033[9m{task["text"]}\033[0m''')
                 i += 1
 
     @query_method
     def list_all_todo(self):
         groups = storage.get_all_groups()
 
+        print("Todo list:")
         for i in range(len(groups)):
             self.render_groups(i)
             self.render_tasks(i)
