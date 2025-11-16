181af9d9d21d620bc788a7b4fa7fd912284966af
Lahanig <lol355467@gmail.com>
2025-11-17 01:41:07 +0300

    Added sqlite db support

--- modules/storage.py
+++ modules/storage.py
@@ -1,7 +1,39 @@
+import sqlite3
+
 class Storage:
+    conn = None
+    cursor = None
+
+    def create_db_connection(self):
+        self.conn = sqlite3.connect('./modules/db/storage.db')
+        self.cursor = self.conn.cursor()
+
+    def close_db_connection_and_commit_changes(self):
+        self.conn.commit()
+        self.conn.close()
+
     def __init__(self):
-        pass
-    
+        self.create_db_connection()
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS groups (
+                id INTEGER PRIMARY KEY,
+                text TEXT,
+                color TEXT
+            )
+        ''')
+        
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY,
+                text TEXT,
+                group_id INTEGER,
+                is_completed BOOLEAN
+            )
+        ''')
+
+        self.close_db_connection_and_commit_changes()
+
     def get_all_groups(self):
         return [{"id": 0, "text": "Важно", "color": "#ff0000"}, {"id": 1, "text": "Менее важно", "color": "ffff00"}, {"id": 2, "text": "Не забыть", "color": "00ff00"}]
 
