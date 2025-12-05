bf58caa57d5b702cd79b3a105f8dce6219d9858e
Lahanig <lol355467@gmail.com>
2025-12-06 00:40:31 +0300

    Updated help query

--- modules/query_methods/list_all_query.py
+++ modules/query_methods/list_all_query.py
@@ -2,4 +2,10 @@ from modules.query_methods.query_method_decorator import query_method
 
 @query_method
 def list_all_query():
-    print("query list: ...")
+    print("Query list: ")
+    print(' help - print query(command) list')
+    print(' ls - print groups and tasks list')
+    print(' clear - clear app screen')
+    print(' welcome - print welcome screen')
+    print(' create - create new group/task')
+    print(' exit - exit')
