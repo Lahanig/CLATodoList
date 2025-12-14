17d5f74196713bac9a5805e221ecb2b2061719a1
Lahanig <lol355467@gmail.com>
2025-12-14 05:25:40 +0300

    Added user inpur history

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 # module_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'modules', 'co'))
 import sys
+import readline
+import os
+import atexit
+
 from modules.storage import storage_instance as storage
 from modules.ui import UI_instance as UI
 from modules.utility import utility_instance as utility
+
+history_file = os.path.join(storage.current_home_path, '.my_app_history')
+
+def save_history():
+    """Saves the current session's history to a file."""
+    try:
+        readline.write_history_file(history_file)
+    except IOError:
+        pass
+        #print(f"Warning: Could not write history file to {history_file}")
+
+def load_history():
+    """Loads history from a file if it exists."""
+    if os.path.exists(history_file):
+        try:
+            readline.read_history_file(history_file)
+        except IOError:
+            print(f"Warning: Could not read history file from {history_file}")
+
+# Load the history when the script starts
+load_history()
+
+# Register the save_history function to be called automatically when the script exits
+atexit.register(save_history)
 
 def main(): 
     utility.clear_screen()
 
     while True:
-        UI.invoke_query_method()
-        UI.query()
+        try:
+            UI.invoke_query_method()
+            UI.query()
+        except (KeyboardInterrupt, EOFError):
+            break
 
 if __name__ == "__main__": 
     if len(sys.argv) > 1 and sys.argv[1].lower() == "dev":
--- modules/storage.py
+++ modules/storage.py
@@ -1,24 +1,27 @@
 import sqlite3
 import os
+
 from pathlib import Path
 
 class Storage:
     conn = None
     cursor = None
     is_dev_mode = False
+    current_home_path = None
 
     def create_db_connection(self):
-        temp_storage_path = "./modules/db"
+        temp_storage_path = "./modules/"
         
         if self.is_dev_mode == False:
             temp_path = Path.home()
 
-            temp_storage_path = f"{temp_path}/CLATodoList/db"
+            temp_storage_path = f"{temp_path}/CLATodoList/"
 
         if not os.path.exists(temp_storage_path):
             os.makedirs(temp_storage_path)
 
-        self.conn = sqlite3.connect(temp_storage_path + '/storage.db')
+        self.current_home_path = temp_storage_path
+        self.conn = sqlite3.connect(self.current_home_path + 'db/storage.db')
         self.cursor = self.conn.cursor()
 
     def close_db_connection_and_commit_changes(self):
